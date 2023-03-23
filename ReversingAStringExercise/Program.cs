69989605b95d4e9491204aaff2dc4a562c4f75ce
andulica <75125226+andulica@users.noreply.github.com>
2023-03-23 05:36:31 +0000

    Issue #7 fixed by changing the format of the for loop:  - had to change reverse from char to String - got rid of counter variable and used only the Lenght method and the i variable to reverse the inputUser
    Also I deleted a few whitespaces

--- ReversingAStringExercise/Program.cs
+++ ReversingAStringExercise/Program.cs
@@ -5,31 +5,26 @@ namespace ReversingAStringExercise
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Please enter the word that you want to reverse: ");
             string inputUser = Console.ReadLine().ToLower();
 
-            char[] reverse = new char[inputUser.Length];
-            int counter = inputUser.Length - 1;
-
-            for (int i = 0; i < inputUser.Length; i++)
-            {
-                reverse[counter] = inputUser[i];
-                counter--;
-            }
-         
+            string reverse = "";
             
+            for (int i = 1; i <= inputUser.Length; i++)
+            {
+                reverse += inputUser[inputUser.Length - i];
+            }
+                
             bool palindrome = true;
 
-            for (int i = 0; i < inputUser.Length; i++)
+            for (int i = 0; i < inputUser.Length -1; i++)
             {         
                 if (inputUser[i] != reverse[i])
                 {
                     palindrome = false;
                 }
             }
-                     
-            
+                               
             if (palindrome)
                       
             {
